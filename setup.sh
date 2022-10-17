7d3220f5f3949b9f05c31eaad75b86d6828eb32a
Dennis Pater <d.pater1986@gmail.com>
2022-10-17 23:41:18 +0200

    FIX softlink give me an error

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ln -s $path/vimrc $HOME/.vimrc
 
 mkdir -p ~/.vim 
 
-ln -s $path/vim/autoload $HOME/.vim/autoload
-ln -s $path/vim/after $HOME/.vim/after
+ln -s $path/vim/autoload/ $HOME/.vim/autoload
+ln -s $path/vim/after/ $HOME/.vim/after
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
